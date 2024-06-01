46b3f17b83a5a2c9fa41519dd84def18c949f3c3
Maximiliano Fernández <thebluemax13@gmail.com>
2024-06-01 10:03:00 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kodi-reactive',
+  webDir: 'www'
+};
+
+export default config;
