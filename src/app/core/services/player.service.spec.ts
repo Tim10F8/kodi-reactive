aaec07ecc1cb72720ca4fa75242337e0494ec4a2
Maximiliano Fernández <thebluemax13@gmail.com>
2025-04-11 19:51:50 +0200

    gte for album square component

--- src/app/core/services/player.service.spec.ts
+++ src/app/core/services/player.service.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { PlayerService } from './player.service';
 import { PipesModule } from '../pipes/pipes-module';
 import { HttpClient } from '@angular/common/http';
@@ -18,32 +21,30 @@ describe('PlayerService', () => {
     await TestBed.compileComponents();
     service = TestBed.inject(PlayerService);
     httpClient = TestBed.inject(HttpClient);
-  //  service = injector.get();
+    //  service = injector.get();
     httpMock = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
     httpMock.verify();
-  });  
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   it('should have uriMediaPlayer', () => {
-    
     expect(service.uriMediaPlayer).toBe('http://localhost:8008/jsonrpc');
   });
-  
-  it('should getAlbum must have the right payload', async() => {
-      const mockResponse = { /* mock response object */ };
-      service.getAlbums().subscribe(response => {
-        expect(response).toEqual(mockResponse);
-      });
-      const req = await httpMock.expectOne('http://localhost:8008/jsonrpc');
-      
-      expect(req.request.method).toBe('POST');
-      req.flush({});
-     httpMock.verify();
+
+  it('should getAlbum must have the right payload', async () => {
+    const mockResponse = {
+      /* mock response object */
+    };
+    const req = await httpMock.expectOne('http://localhost:8008/jsonrpc');
+
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+    httpMock.verify();
   });
 });
